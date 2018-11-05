d26083170d45693f684f28c595f148bbdf676217
Stefano Tosi Butori <butori@gmail.com>
2018-11-05 17:06:04 -0200

    Added Device, User and Booking models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_175144) do
+ActiveRecord::Schema.define(version: 2018_11_05_184117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "device_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.float "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["device_id"], name: "index_bookings_on_device_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "devices", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "brand"
+    t.string "model"
+    t.string "category"
+    t.float "price"
+    t.boolean "is_rented"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "photo"
+    t.index ["user_id"], name: "index_devices_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +50,15 @@ ActiveRecord::Schema.define(version: 2018_11_05_175144) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.boolean "is_owner"
+    t.string "location"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "devices"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "devices", "users"
 end
