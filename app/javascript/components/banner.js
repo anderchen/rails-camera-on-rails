f18d8a198a926f6bc8d266a07614a85b224daccc
Nicolas deCamaret <nicolasdec@gmail.com>
2018-11-05 19:47:02 -0200

    Fixed logo, BG

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,8 +2,11 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["camera...", "lens...", "tripod...", "drone..."],
-    typeSpeed: 40,
+    strings: ["camera...", "lens...", "tripod...", "drone...", "SD Card..."],
+    typeSpeed: 70,
+    backSpeed: 40,
+    shuffle: true,
+    showCursor: true,
     loop: true
   });
 }
