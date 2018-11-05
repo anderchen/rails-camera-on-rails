2a7401111e0df29252d608c60bcae9a331b80d62
Anderson Chen <ander.chen00@gmail.com>
2018-11-05 18:33:49 -0200

    Added validations to the models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,6 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :user_id, presence: true
+  validates :device_id, presence: true
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -5,7 +5,7 @@ class Device < ApplicationRecord
   validates :description, presence: true
   validates :brand, presence: true
   validates :model, presence: true
-  validates :category, presence: true
-  validates :price, presence: true
-  validates :is_rented, presence: true
+  validates :category, presence: true, inclusion: { in: %w(Cameras Lens Tripods Drones Filters Lighting Accessories Other)}
+  validates :price, presence: true, format:{ with: /\A\d+(?:\.\d{2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000}
+  validates :is_rented, presence: true, inclusion: {in: [true, false]}, default: false
 end
