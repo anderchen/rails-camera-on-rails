0ead6671159a9edfcb628cc8cbf537e3523630ad
Anderson Chen <ander.chen00@gmail.com>
2018-11-06 11:49:19 -0200

    Corrected the migrations and schema for User

--- db/migrate/20181105184033_add_columns_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :is_owner, :boolean
-    add_column :users, :location, :string
-    add_column :users, :photo, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_184117) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.boolean "is_owner"
-    t.string "location"
+    t.text "location"
     t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
